ca2627a4597e137a9272d205f42b12d1e05cb2e7
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-17 10:25:41 +0200

    [UPD] Added v-for projects and image control

--- src/App.vue
+++ src/App.vue
@@ -26,9 +26,9 @@ export default {
         </div>
         <div class="col-12">
           <div class="row">
-            <div class="col-12">
+            <div class="col-12" v-for="proj in projects" :key="proj.id">
               <div class="card">
-                <img class="card-img-top" src="">
+                <img class="card-img-top" :src="proj.image != null ? `http://127.0.0.1:8000/storage/${proj.image}` : 'https://placehold.co/600x400?text=Immagine+copertina'">
                 <div class="card-body">
                   <h4 class="card-title">Titolo</h4>
                   <p>lorem ipsum</p>
