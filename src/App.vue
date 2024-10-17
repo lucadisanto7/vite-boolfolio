73f8f5c82f8245dd5dbb5cfb632826aa8c1cb367
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-17 10:13:20 +0200

    [UPD] Added content

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,15 @@ export default {
 }
 </script>
 <template lang="">
-  <div>
-    
+  <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="text-center"> Elenco progetti</h1>
+        </div>
+        <div class="col-12">
+
+        </div>
+      </div>
   </div>
 </template>
 <style lang="scss">
