8e1201c7c67950aeaa1c8b954c6f3b0bcd38d650
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-17 10:15:16 +0200

    [UPD] Added get all projects

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script>
+import axios from 'axios';
 export default {
-  
+  data(){
+    return{
+      projects:[]
+    }
+  },
+  methods:{
+    getAllProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects')
+
+    }
+  }
 }
 </script>
 <template lang="">
