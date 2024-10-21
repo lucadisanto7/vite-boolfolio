fdf2886cc4f3e328e4124438ce8b0f51e007919c
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-21 21:03:35 +0200

    [UPD] Added router.js

---
+++ src/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import PageHome from './pages/PageHome.vue';
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path :'/',
+            name: 'homepage',
+            component: PageHome
+        }
+    ]
+})
+
+export { router }
