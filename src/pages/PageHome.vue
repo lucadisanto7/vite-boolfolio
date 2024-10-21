851f1d38984fec6335981b220f611c9b76b02238
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-21 21:02:22 +0200

    [UPD] Added page home

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <h1 class="text-center my-5">Laravel Boolfolio</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum, nobis neque? Facere pariatur a velit expedita, earum ullam, at fuga harum accusantium veniam nemo eveniet temporibus omnis placeat odio saepe.</p>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
