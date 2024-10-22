0538e8cadcb596958f73f07349bb8d1303dd6887
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-23 00:15:31 +0200

    [UPD] Added v-model

--- src/pages/PageContacts.vue
+++ src/pages/PageContacts.vue
@@ -17,23 +17,23 @@ export default {
                         <div class="row gy-3">
                             <div class="col-12 col-md-6">
                                 <label class="control-label">Nome</label>
-                                <input type="text" class="form-control" name="name" id="name" placeholder="name">
+                                <input type="text" class="form-control" name="name" id="name" v-model="name" placeholder="name">
                             </div>
                             <div class="col-12 col-md-6">
                                 <label class="control-label">Cognome</label>
-                                <input type="text" class="form-control" name="surname" id="surname" placeholder="surname">
+                                <input type="text" class="form-control" name="surname" id="surname" v-model="surname" placeholder="surname">
                             </div>
                             <div class="col-12 col-md-6">
                                 <label class="control-label">Email</label>
-                                <input type="text" class="form-control" name="email" id="email" placeholder="email">
+                                <input type="text" class="form-control" name="email" id="email" v-model="email" placeholder="email">
                             </div>
                             <div class="col-12 col-md-6">
                                 <label class="control-label">Telefono</label>
-                                <input type="text" class="form-control" name="phone" id="phone" placeholder="phone">
+                                <input type="text" class="form-control" name="phone" id="phone" v-model="phone" placeholder="phone">
                             </div>
                             <div class="col-12 col-md-6">
                                 <label class="control-label">Contenuto</label>
-                                <textarea type="text" class="form-control" name="content" id="content" placeholder="content"></textarea>
+                                <textarea type="text" class="form-control" name="content" id="content" v-model="content" placeholder="content"></textarea>
                             </div>
                         </div>
                         <div class="col-12">
