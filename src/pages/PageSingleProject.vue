14e34d76613edde40a0461a42395b0b2c8c87cf1
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-22 23:33:44 +0200

    [UPD] Added content to image, type and technologies

--- src/pages/PageSingleProject.vue
+++ src/pages/PageSingleProject.vue
@@ -25,13 +25,18 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col-12 col-md-6 col-lg-4">
-                    <img src="" />
+                    <img :src="project.image !=null ? `http://127.0.0.1:8000/storage/${project.image}`: 'https://placehold.co/600x400?'" />
                 </div>
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="content">
                         <h1>Titolo</h1>
-                        <p><strong>Tipologia:</strong> Tipo</p>
-                        <p><strong>Tecnologie:</strong> Tecnologie</p>
+                        <p><strong>Tipologia:</strong> {{project.type != null ? project.type.name : 'nessuna tipologia'}}</p>
+                        <p v-if="project.technologies.lenght > 0">
+                            <strong>Tecnologie:</strong> 
+                            <span class="pe-2" v-for="tech in project.technologies">
+                                {{ tech.name }}
+                            </span>
+                        </p>
                     </div>
                 </div>
                 <div class="col-12">
