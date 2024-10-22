ac68f6c917e6e628ff512b86d692bdc1270749cc
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-23 00:20:59 +0200

    [UPD] Imported page contacts route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import PageHome from './pages/PageHome.vue';
 import PagePortfolio from './pages/PagePortfolio.vue';
 import PageSingleProject from './pages/PageSingleProject.vue';
+import PageContacts from './pages/PageContacts.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -19,7 +20,13 @@ const router = createRouter({
             path: '/portfolio',
             name: 'portfolio',
             component: PagePortfolio
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: PageContacts,
         }
+
     ]
 })
 
