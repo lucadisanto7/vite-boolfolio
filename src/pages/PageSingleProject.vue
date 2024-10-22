0929cd9a9f9e308256c7c7253e1c70dc7f662f0d
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-22 23:07:20 +0200

    [UPD] Added content

--- src/pages/PageSingleProject.vue
+++ src/pages/PageSingleProject.vue
@@ -1,11 +1,31 @@
 <script>
 export default {
-    
+    data(){
+        return{
+
+        }
+    }
 }
 </script>
 <template>
     <div>
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <img src="" />
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="content">
+                        <h1>Titolo</h1>
+                        <p><strong>Tipologia:</strong> Tipo</p>
+                        <p><strong>Tecnologie:</strong> Tecnologie</p>
+                    </div>
+                </div>
+                <div class="col-12">
+                    
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <style lang="">
