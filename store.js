caa7d6437af429b3d39b96059f89e9383cf7bd4b
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-22 23:23:59 +0200

    [UPD} Added store

---
+++ store.js
@@ -0,0 +1,14 @@
+import { reactive } from 'vue';
+export const store = reactive({
+    baseUrl: 'http://127.0.0.1:8000/api',
+    menuItems:[
+        {
+            name: 'homepage',
+            label: 'Home',
+        },
+        {
+            name: 'portfolio',
+            label: 'portfolio',
+        }
+    ]
+});
