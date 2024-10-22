e46f9997b1da5919d5af56421b18b2304a330699
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-22 23:13:19 +0200

    [UPD] Imported axios and store

--- src/pages/PageSingleProject.vue
+++ src/pages/PageSingleProject.vue
@@ -1,8 +1,12 @@
 <script>
+
+import axios from'axios';
+import { store } from 'store.js';
 export default {
     data(){
         return{
-
+            store,
+            project: null
         }
     },
     methods:{
